a4008ae8430b2ae51b32d30200dcdd7360030b0d
dura-amar <dura.email@gmail.com>
2022-07-17 22:07:48 +0545

    comtract draft completed

--- contracts/Workshop.sol
+++ contracts/Workshop.sol
@@ -34,17 +34,30 @@ contract WorkshopChain {
         string time;
         uint fee;
         address organizer;
-        Participant[]  participants;
+        Participant[] participants;
     }
 
     Workshop[] public workshops;
     mapping(address => Organizer) public organizers;
     mapping(address => Participant) public participants;
 
-
-
-
-
+    /*
+     * Modifiers
+     */
+    modifier onlyOrganizer() {
+        bytes memory name = bytes(organizers[msg.sender].name);
+        require(name.length > 0, "You are not registered as organizer.");
+        _;
+    }
+    modifier onlyParticipant() {
+        bytes memory name = bytes(participants[msg.sender].name);
+        require(name.length > 0, "You are not registered as participant.");
+        _;
+    }
+    modifier requireFree(Workshop memory w){
+        require(msg.value>=(w.fee),"Please send enough fee for the workshop.");
+        _;
+    }
 
     // Default constructor to set the owner address only
     constructor() {
@@ -65,8 +78,10 @@ contract WorkshopChain {
      * @param : name, email
      * adds a new Participant to participants
      */
-    function addParticipant(string memory _name, string memory _email) external {
-        participants[msg.sender] =Participant(msg.sender, _name, _email);
+    function addParticipant(string memory _name, string memory _email)
+        external
+    {
+        participants[msg.sender] = Participant(msg.sender, _name, _email);
     }
 
     /*
@@ -86,15 +101,15 @@ contract WorkshopChain {
     function addWorshop(
         string memory _bannerUri,
         string memory _title,
-        uint  _startDate,
-        uint  _endDate,
+        uint _startDate,
+        uint _endDate,
         string memory _venue,
         string memory _time,
         uint _fee
-    ) external {
+    ) external onlyOrganizer {
         // get a reference to the pushed struct
         // then modify the storage instance
-        Workshop storage w1= workshops.push();
+        Workshop storage w1 = workshops.push();
         w1.bannerUri = _bannerUri;
         w1.title = _title;
         w1.startDate = _startDate;
@@ -106,36 +121,60 @@ contract WorkshopChain {
     }
 
     /*
-    * Organizer functions
-    * 
-    */
+     * Organizer functions
+     *
+     */
 
     /*
-    * @param : workshopIndex
-    * Returns list of participants for the workshop
-    */
-    function getParticipants(uint _workshopIndex) external view returns(Participant[] memory){
-        Workshop memory w1=workshops[_workshopIndex];
+     * @param : workshopIndex
+     * Returns list of participants for the workshop
+     */
+    function getParticipants(uint _workshopIndex)
+        external
+        view
+        onlyOrganizer
+        returns (Participant[] memory)
+    {
+        Workshop memory w1 = workshops[_workshopIndex];
         return w1.participants;
+    }
+
+    function getMyWorkshops() external view returns (Workshop[] memory) {
+        uint len = workshops.length;
+        Workshop[] memory ws = new Workshop[](len);
+        uint i = 0;
+        uint wi = 0;
+        for (i; i < len; i++) {
+            if (workshops[i].organizer == msg.sender) {
+                ws[wi] = workshops[i];
+                wi++;
+            }
+        }
+        return ws;
     }
 
     // TODO : organizer sendCertificates
 
     /*
-    * Participants functions
-    */
+     * Participants functions
+     */
 
     /*
-    * returns all the current workshops
-    */
-    function getCurrentWorkshops() external view returns(Workshop[] memory){
-        uint len=workshops.length;
-        Workshop[] memory ws=new Workshop[](len);
-        uint i=0;
-        uint wi=0;
-        for(i;i<len;i++){
-            if(workshops[i].endDate<block.timestamp){
-                ws[wi]=workshops[i];
+     * returns all the current workshops
+     */
+    function getCurrentWorkshops()
+        external
+        view
+        onlyParticipant
+        returns (Workshop[] memory)
+    {
+        uint len = workshops.length;
+        Workshop[] memory ws = new Workshop[](len);
+        uint i = 0;
+        uint wi = 0;
+        for (i; i < len; i++) {
+            if (workshops[i].endDate < block.timestamp) {
+                ws[wi] = workshops[i];
                 wi++;
             }
         }
@@ -143,18 +182,19 @@ contract WorkshopChain {
     }
 
     /*
-    * register for workshop
-    * @param: _workshopIndex
-    *
-    */
+     * register for workshop
+     * @param: _workshopIndex
+     *
+     */
 
-    function registerForWorkshop(uint _workshopIndex)external{
+    function registerForWorkshop(uint _workshopIndex) external onlyParticipant requireFree(workshops[_workshopIndex]) payable{
         //check values
 
-        Workshop memory w1=workshops[_workshopIndex];
-        uint pLen=w1.participants.length;
-        workshops[_workshopIndex].participants[pLen]=(participants[msg.sender]);
+        Workshop memory w1 = workshops[_workshopIndex];
+        uint pLen = w1.participants.length;
+        workshops[_workshopIndex].participants[pLen] = (
+            participants[msg.sender]
+        );
     }
     //TODO : request for certificate
-
 }
