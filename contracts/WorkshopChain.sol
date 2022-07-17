d086ae80a92d203072dea49f1e216c313058a069
dura-amar <dura.email@gmail.com>
2022-07-17 23:17:12 +0545

    React functions created

---
+++ contracts/WorkshopChain.sol
@@ -0,0 +1,201 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract WorkshopChain {
+    address owner;
+
+    // This enum is for different user types
+    enum UserType {
+        ORGANIZER,
+        PARTICIPANT
+    }
+
+    //struct for organizer
+    struct Organizer {
+        address id;
+        string name;
+        string about;
+    }
+
+    //struct for participant
+    struct Participant {
+        address id;
+        string name;
+        string email;
+    }
+
+    //struct for workshop
+    struct Workshop {
+        string bannerUri; //hash of the banner stored in ipfs
+        string title;
+        uint startDate;
+        uint endDate;
+        string venue;
+        string time;
+        uint fee;
+        address organizer;
+        Participant[] participants;
+    }
+
+    Workshop[] public workshops;
+    mapping(address => Organizer) public organizers;
+    mapping(address => Participant) public participants;
+
+    /*
+     * Modifiers
+     */
+    modifier onlyOrganizer() {
+        bytes memory name = bytes(organizers[msg.sender].name);
+        require(name.length > 0, "You are not registered as organizer.");
+        _;
+    }
+    modifier onlyParticipant() {
+        bytes memory name = bytes(participants[msg.sender].name);
+        require(name.length > 0, "You are not registered as participant.");
+        _;
+    }
+    // modifier requireFree(Workshop memory w){
+    //     require(msg.value>=(w.fee),"Please send enough fee for the workshop.");
+    //     _;
+    // }
+
+    // Default constructor to set the owner address only
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /*
+     * add organizer
+     * @param : name, about
+     * adds a new Organizer to organizers
+     */
+    function addOrganizer(string memory _name, string memory _about) external {
+        organizers[msg.sender] = Organizer(msg.sender, _name, _about);
+    }
+
+    /*
+     * add participant
+     * @param : name, email
+     * adds a new Participant to participants
+     */
+    function addParticipant(string memory _name, string memory _email)
+        external
+    {
+        participants[msg.sender] = Participant(msg.sender, _name, _email);
+    }
+
+    /*
+     * add workshop
+     * @param : 
+            bannerUri,
+            title,
+            startDate,
+            endDate,
+            venue,
+            time,
+            fee
+     * adds a new Workshop to workshops
+     * called by organizer only
+     */
+
+    function addWorkshop(
+        string memory _bannerUri,
+        string memory _title,
+        uint _startDate,
+        uint _endDate,
+        string memory _venue,
+        string memory _time,
+        uint _fee
+    ) external onlyOrganizer {
+        // get a reference to the pushed struct
+        // then modify the storage instance
+        Workshop storage w1 = workshops.push();
+        w1.bannerUri = _bannerUri;
+        w1.title = _title;
+        w1.startDate = _startDate;
+        w1.endDate = _endDate;
+        w1.venue = _venue;
+        w1.time = _time;
+        w1.fee = _fee;
+        w1.organizer = msg.sender;
+    }
+
+    /*
+     * Organizer functions
+     *
+     */
+
+    /*
+     * @param : workshopIndex
+     * Returns list of participants for the workshop
+     */
+    function getParticipants(uint _workshopIndex)
+        external
+        view
+        onlyOrganizer
+        returns (Participant[] memory)
+    {
+        Workshop memory w1 = workshops[_workshopIndex];
+        return w1.participants;
+    }
+
+    function getMyWorkshops() external view onlyOrganizer returns (Workshop[] memory) {
+        uint len = workshops.length;
+        Workshop[] memory ws = new Workshop[](len);
+        uint i = 0;
+        uint wi = 0;
+        for (i; i < len; i++) {
+            if (workshops[i].organizer == msg.sender) {
+                ws[wi] = workshops[i];
+                wi++;
+            }
+        }
+        return ws;
+    }
+
+    // TODO : organizer sendCertificates
+
+    /*
+     * Participants functions
+     */
+
+    /*
+     * returns all the current workshops
+     */
+    function getCurrentWorkshops()
+        external
+        view
+        onlyParticipant
+        returns (Workshop[] memory)
+    {
+        uint len = workshops.length;
+        Workshop[] memory ws = new Workshop[](len);
+        uint i = 0;
+        uint wi = 0;
+        for (i; i < len; i++) {
+            if (workshops[i].endDate < block.timestamp) {
+                ws[wi] = workshops[i];
+                wi++;
+            }
+        }
+        return ws;
+    }
+
+    /*
+     * register for workshop
+     * @param: _workshopIndex
+     *
+     */
+
+    function registerForWorkshop(uint _workshopIndex) external onlyParticipant payable{
+        //check values
+        Workshop memory w1= workshops[_workshopIndex];
+        require(msg.value>=w1.fee,"Please send enough fee for the workshop.");
+
+        uint pLen = w1.participants.length;
+        workshops[_workshopIndex].participants[pLen] = (
+            participants[msg.sender]
+        );
+    }
+    //TODO : request for certificate
+}
--- src/App.js
+++ src/App.js
@@ -6,21 +6,16 @@ import logo from './logo.svg';
 import './App.css';
 
 //importing abi of our contract
-import Certificates from './artifacts/contracts/Certificates.sol/Certificates.json';
+import WorkshopChain from './artifacts/contracts/WorkshopChain.sol/WorkshopChain.json';
 
 //storing our contract in a variable
 //address obtained after depoying the contract script
-const certificatesAddress="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const workshopAddress="0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 
 
 
 function App() {
-
-
-
-  // storing greeting in local state
-  const [userType] = useState();
 
   //request access to the user's MetaMask account
   // use this with connect wallet button
@@ -28,27 +23,103 @@ function App() {
     await window.ethereum.request({method: 'eth_requestAccounts'});
   }
 
-
-  async function getIssuers(){
+  // get functions
+  async function getCurrentWorkshops(){
     if (typeof window.ethereum !== 'undefined') {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(certificatesAddress,Certificates.abi, provider)
+      const contract = new ethers.Contract(workshopAddress,WorkshopChain.abi, provider)
       try {
-        const data = await contract.getIssuers()
-        console.log('data: ', data)
+        const currentWorkshops = await contract.getCurrentWorkshops();
+        console.log('Current Workshop: ', currentWorkshops)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }   
+  }
+
+
+  async function getMyWorkshops(){
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(workshopAddress,WorkshopChain.abi, provider)
+      try {
+        const myWorkshops = await contract.getMyWorkshops()
+        console.log('My Workshops: ', myWorkshops)
       } catch (err) {
         console.log("Error: ", err)
       }
     }    
   }
 
+  async function getParticipants(workshopIndex){
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(workshopAddress,WorkshopChain.abi, provider)
+      try {
+        const participants = await contract.getParticipants(workshopIndex)
+        console.log('My Workshops: ', participants)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
 
+  //add functions
+  async function addOrganizer(name,about) {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(workshopAddress, WorkshopChain.abi, signer)
+      const transaction = await contract.addOrganizer(name,about)
+      await transaction.wait()
+    }
+  }
+
+  async function addParticipant(name,email) {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(workshopAddress, WorkshopChain.abi, signer)
+      const transaction = await contract.addParticipant(name,email)
+      await transaction.wait()
+    }
+  }
+
+  async function addWorkshop(banneruri,title,startDate,endDate,venu,time,fee) {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(workshopAddress, WorkshopChain.abi, signer)
+      const transaction = await contract.addWorkshop(banneruri,title,startDate,endDate,venu,time,fee)
+      await transaction.wait()
+    }
+  }
+  async function registerForWorkshopR(workshopIndex) {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(workshopAddress, WorkshopChain.abi, signer)
+      const transaction = await contract.registerForWorkshop(workshopIndex)
+      await transaction.wait()
+    }
+  }
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={requestAccount}>Connect Wallet</button>
-        <button onClick={getIssuers}>Get Issuers</button>
+        <button onClick={getCurrentWorkshops}>Participant: Get Current Workshops</button>
+        <button onClick={getMyWorkshops}>Organizer: Ge my workshops</button>
+        <button onClick={getParticipants}>Participants</button>
+        <button onClick={addOrganizer}>Add Organizer</button>
+        <button onClick={addParticipant}>Add Participant</button>
+        <button onClick={addWorkshop}>Add workshops</button>
+      
       </header>
     </div>
   );
